389f4a05eef3e5a73d8b0adc672a00c998ebc367
myata7 <myata7@my.smccd.edu>
2026-05-12 15:42:18 -0700

    Fixed comments page route and added reusable menu

--- calofficials/src/main/java/api/controller/CommentsPageController.java
+++ calofficials/src/main/java/api/controller/CommentsPageController.java
@@ -3,21 +3,18 @@ package api.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 
 @Controller
-@RequestMapping(value = "/comments")
 public class CommentsPageController {
-     /**
+
+    /**
      * Method connects specified url to an html page to view (comments.html)
      * @param model information/element to be added to the page
      * @return html file name to load
      */
     @GetMapping("/comments")
     public String commentsPage(Model model) {
-        model.addAttribute("message","See what people are saying");
+        model.addAttribute("message", "See what people are saying");
         return "comments";
     }
-    
 }
