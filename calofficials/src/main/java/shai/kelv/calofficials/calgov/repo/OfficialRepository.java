f2e968a2f74b98d8edd9b4decb6dd59f4ae17f6c
shailita <shailakailani@gmail.com>
2026-05-15 13:47:20 -0700

    menu fix

--- calofficials/src/main/java/shai/kelv/calofficials/calgov/repo/OfficialRepository.java
+++ calofficials/src/main/java/shai/kelv/calofficials/calgov/repo/OfficialRepository.java
@@ -3,22 +3,14 @@
  * @author Shaila Lewis
  * @since 05.07.2026
  */
-<<<<<<< HEAD
-package shai.kelv.calofficials.calgov.repo;
-
-import java.util.List;
-
-=======
 
 package shai.kelv.calofficials.calgov.repo;
 import shai.kelv.calofficials.calgov.entity.Official;
->>>>>>> ebabe6e4fee33caf1f25ce4d4f93d2e9b1a0d353
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import java.util.List;
 
-import shai.kelv.calofficials.calgov.entity.Official;
-import shai.kelv.calofficials.calgov.entity.OfficialType;
 
 public interface OfficialRepository extends JpaRepository<Official, Long> {
 
